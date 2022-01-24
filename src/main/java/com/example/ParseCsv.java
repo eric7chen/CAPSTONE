80d984f8e09a0352b333e7dc21fd871bab43c9ca
Nick <nicholas.j.thompson-1@ou.edu>
2022-01-24 14:15:15 -0600

    implement argument pass of csv and unit tests

--- src/main/java/com/example/ParseCsv.java
+++ src/main/java/com/example/ParseCsv.java
@@ -12,45 +12,32 @@ public class ParseCsv {
     private static String COMMA_DELIMITER = ",";
 
     public static void main(String[] args) {
-        List<List<String>> records = new ArrayList<>();
+        if (args.length < 1) {
+            System.err.println("requires 1 argument of csv file");
+            return;
+        }
+
+        List<List<String>> records = new ArrayList<List<String>>();
+        String csv = args[0];
         
-        try (BufferedReader reader = new BufferedReader(new FileReader("English-Grid view.csv"));) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(csv))) {
            String line;
            while((line = reader.readLine()) != null) {
                String[] values = line.split(COMMA_DELIMITER);
                records.add(Arrays.asList(values));
            }
-        } catch (IOException e) {
-            System.err.println(e);
-        }
-
-        for (List<String> list : records) {
-            for (String str : list) {
-                System.out.print(str + ", ");
-            }
-            System.out.println();
-        }
-
-        System.out.println();
-
-        List<List<String>> esRecords = new ArrayList<>();
-        
-        try (BufferedReader reader = new BufferedReader(new FileReader("Spanish-Grid view.csv"));) {
-           String line;
-           while((line = reader.readLine()) != null) {
-               String[] values = line.split(COMMA_DELIMITER);
-               esRecords.add(Arrays.asList(values));
+           reader.close();
+           
+           for (List<String> record : records) {
+               for (String entry : record) {
+                   System.out.print(entry + ",");
+               }
+               System.out.print("\n");
            }
         } catch (IOException e) {
-            System.err.println(e);
+            System.err.println(e.toString());
         }
 
-        for (List<String> list : esRecords) {
-            for (String str: list) {
-                System.out.print(str + ", ");
-            }
-            System.out.println();
-        }
-        
+
     }
 }
